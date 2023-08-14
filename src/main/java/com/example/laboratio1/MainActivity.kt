f2d0a99b4706fbb79614995ebad84c386b3aa3ec
GersonRamos001 <ramosargueta@gmail.com>
2023-08-14 10:32:02 -0600

    list is ready

--- src/main/java/com/example/laboratio1/MainActivity.kt
+++ src/main/java/com/example/laboratio1/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,11 +14,17 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.painterResource
@@ -24,56 +32,105 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.example.laboratio1.ui.theme.Laboratio1Theme
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Laboratio1Theme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    MessageCard(Message("Ejecutable","Body Ejecutable"))
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    Conversation(SampleData.conversationSample)
                 }
             }
         }
     }
 }
 
-data class Message(val author: String, val body: String)
+
+    data class Message(val author: String, val body: String)
 
 @Composable
 fun MessageCard(msg: Message) {
-    // Add padding around our message
     Row(modifier = Modifier.padding(all = 8.dp)) {
         Image(
             painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
+            contentDescription = null,
             modifier = Modifier
-                // Set image size to 40dp
                 .size(40.dp)
-                //clip image to be shaped as circle
                 .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
         )
-        //Add horizontal space between the image and the column
         Spacer(modifier = Modifier.width(8.dp))
 
-        Column{
-            Text(text = msg.author)
-            //add a vertical space between the author and message
+        // We keep track if the message is expanded or not in this
+        // variable
+        var isExpanded by remember { mutableStateOf(false) }
+        // surfaceColor will be updated gradually from one color to the other
+        val surfaceColor by animateColorAsState(
+            if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+        )
+
+        // We toggle the isExpanded variable when we click on this Column
+        Column(modifier = Modifier.clickable { isExpanded = !isExpanded }) {
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colorScheme.secondary,
+                style = MaterialTheme.typography.titleSmall
+            )
+
             Spacer(modifier = Modifier.height(4.dp))
-            Text(text = msg.body)
+
+            Surface(
+                shape = MaterialTheme.shapes.medium,
+                shadowElevation = 1.dp,
+                // surfaceColor color will be changing gradually from primary to surface
+                color = surfaceColor,
+                // animateContentSize will change the Surface size gradually
+                modifier = Modifier.animateContentSize().padding(1.dp)
+            ) {
+                Text(
+                    text = msg.body,
+                    modifier = Modifier.padding(all = 4.dp),
+                    // If the message is expanded, we display all its content
+                    // otherwise we only display the first line
+                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                    style = MaterialTheme.typography.bodyMedium
+                )
+            }
+        }
+    }
+}
+
+
+
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        Laboratio1Theme {
+            Surface {
+                MessageCard(
+                    msg = Message("Gerson", "Take a look at Jetpack Compose, it's great!")
+                )
+            }
         }
     }
 
+@Composable
+fun Conversation(messages: List<Message>) {
+    LazyColumn {
+        items(messages) { message ->
+            MessageCard(message)
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun PreviewMessageCard() {
-    MessageCard(
-        msg = Message("Gerson","Today is a big day to learn Compose")
-    )
+fun PreviewConversation() {
+    Laboratio1Theme {
+        Conversation(SampleData.conversationSample)
+    }
 }
+
---
+++ src/main/java/com/example/laboratio1/SampleData.kt
@@ -0,0 +1,53 @@
+package com.example.laboratio1
+
+/**
+ * SampleData for Jetpack Compose Tutorial 
+ */
+object SampleData {
+    // Sample conversation data
+    val conversationSample = listOf(
+        Message("William Shakespeare", "To be or not to be, that is the question."),
+        Message("Albert Einstein", "Imagination is more important than knowledge."),
+        Message("Leonardo da Vinci", "Simplicity is the ultimate sophistication."),
+        Message("Isaac Newton", "If I have seen further, it is by standing on the shoulders of giants."),
+        Message("Marie Curie", "Nothing in life is to be feared, it is only to be understood."),
+        Message("Nelson Mandela", "It always seems impossible until it's done."),
+        Message("Mahatma Gandhi", "Be the change that you wish to see in the world."),
+        Message("Martin Luther King Jr.", "I have a dream."),
+        Message("Winston Churchill", "Success is not final, failure is not fatal: It is the courage to continue that counts."),
+        Message("Mother Teresa", "Not all of us can do great things. But we can do small things with great love."),
+        Message("William Shakespeare", "To be or not to be, that is the question."),
+        Message("Albert Einstein", "Imagination is more important than knowledge."),
+        Message("Leonardo da Vinci", "Simplicity is the ultimate sophistication."),
+        Message("Isaac Newton", "If I have seen further, it is by standing on the shoulders of giants."),
+        Message("Marie Curie", "Nothing in life is to be feared, it is only to be understood."),
+        Message("Nelson Mandela", "It always seems impossible until it's done."),
+        Message("Mahatma Gandhi", "Be the change that you wish to see in the world."),
+        Message("Martin Luther King Jr.", "I have a dream."),
+        Message("Winston Churchill", "Success is not final, failure is not fatal: It is the courage to continue that counts."),
+        Message("Mother Teresa", "Not all of us can do great things. But we can do small things with great love."),
+        Message("Steve Jobs", "Stay hungry, stay foolish."),
+        Message("Pablo Picasso", "Every child is an artist. The problem is how to remain an artist once he grows up."),
+        Message("Charles Darwin", "It is not the strongest of the species that survive, nor the most intelligent, but the one most responsive to change."),
+        Message("Vincent van Gogh", "I dream my painting and I paint my dream."),
+        Message("Emily Dickinson", "Hope is the thing with feathers that perches in the soul."),
+        Message("Mark Twain", "The two most important days in your life are the day you are born and the day you find out why."),
+        Message("George Orwell", "In a time of deceit, telling the truth is a revolutionary act."),
+        Message("Helen Keller", "The only thing worse than being blind is having sight but no vision."),
+        Message("Oscar Wilde", "Be yourself; everyone else is already taken."),
+        Message("J.K. Rowling", "It does not do to dwell on dreams and forget to live."),
+        Message("Frida Kahlo", "I am my own muse. I am the subject I know best."),
+        Message("Neil Armstrong", "That's one small step for [a] man, one giant leap for mankind."),
+        Message("Coco Chanel", "The best things in life are free. The second best are very expensive."),
+        Message("Stephen Hawking", "Intelligence is the ability to adapt to change."),
+        Message("Jane Austen", "There is no charm equal to tenderness of heart."),
+        Message("Ernest Hemingway", "The world breaks everyone, and afterward, some are strong at the broken places."),
+        Message("Maya Angelou", "I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel."),
+        Message("Rosa Parks", "I would like to be remembered as a person who wanted to be free... so other people would be also free."),
+        Message("Walt Disney", "All our dreams can come true if we have the courage to pursue them."),
+        Message("Bruce Lee", "Be water, my friend."),
+        Message("Audrey Hepburn", "The most important thing is to enjoy your life—to be happy—it's all that matters."),
+        Message("John Lennon", "Imagine all the people living life in peace. You may say I'm a dreamer, but I'm not the only one."
+        ),
+    )
+}
